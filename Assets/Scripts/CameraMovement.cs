aa97ca411997f4c35e17f3e29a40504d829de00c
SneakyKenny <kennycreative@hotmail.fr>
2018-02-04 06:40:53 +0100

    CameraControl
    A short script to let the camera rotate around the earth when right-clicking and dragging the mouse. Also, using the mouse scrollwheel changes the "zoom" of the camera (well, it's changing it's position, but w/e).
    
    PS_1 : Added a start of a script that'll lead to the clickable-earth to select a country.
    PS_2 : Unity got updated, i'm on 2017.3.0.0f3. Get up-to-date you sluts !
    PS_3 : Started using Rider instead of Monodevelop to code (personnal)

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class CameraMovement : MonoBehaviour
+{
+	private bool isAlreadyClicking = false;
+
+	public Transform target;
+
+	private float distance = 10f;
+
+	[Range ( 0f, 250f )] public float xSpeed = 50f;
+	[Range ( 0f, 250f )] public float ySpeed = 50f;
+
+	[Range ( -90f,  0f )] public float minAngle = -10;
+	[Range (   0f, 90f )] public float maxAngle = 25f;
+
+	[Range (  3f, 10f )] public float minDist = 3f;
+	[Range ( 10f, 20f )] public float maxDist = 15f;
+
+	private float x, y;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		var angles = this.transform.eulerAngles;
+		this.x = angles.x;
+		this.y = angles.y;
+
+		this.rb = GetComponent <Rigidbody> ();
+		if ( this.rb != null )
+		{
+			this.rb.freezeRotation = true;
+		}
+	}
+
+	void Update ()
+	{
+		if ( !this.isAlreadyClicking && Input.GetButtonDown ( "Fire2" ) )
+		{
+			this.isAlreadyClicking = true;
+		}
+		if ( this.isAlreadyClicking && Input.GetButtonUp ( "Fire2" ) )
+		{
+			this.isAlreadyClicking = false;
+		}
+		if ( Input.GetButton ( "Fire2" ) )
+		{
+			DragCamera ();
+		}
+	}
+
+	private void DragCamera ()
+	{
+		if ( !this.isAlreadyClicking )
+		{
+			return;
+		}
+
+		if ( this.target )
+		{
+			this.x += Input.GetAxis ( "Mouse X" ) * this.xSpeed * this.distance * 0.02f;
+			this.y -= Input.GetAxis ( "Mouse Y" ) * this.ySpeed * 0.02f;
+
+			this.y = ClampAngle ( this.y, this.minAngle, this.maxAngle );
+			
+			Quaternion rotation = Quaternion.Euler ( this.y, this.x, 0f );
+
+			this.distance = Mathf.Clamp ( 
+				this.distance - Input.GetAxis ( "Mouse ScrollWheel" ) * 5f, 
+				this.minDist, 
+				this.maxDist 
+			);
+
+			RaycastHit hit;
+			
+			if ( Physics.Linecast ( this.target.position, this.transform.position, out hit ) )
+			{
+				this.distance -= hit.distance;
+			}
+			
+			Vector3 negDistance = new Vector3 ( 0.0f, 0.0f, -this.distance );
+			Vector3 position = rotation * negDistance + this.target.position;
+
+			this.transform.rotation = rotation;
+			this.transform.position = position;
+		}
+	}
+
+	public static float ClampAngle ( float angle, float min, float max )
+	{
+		if ( angle < -360f )
+			angle += 360f;
+		if ( angle > 360f )
+			angle -= 360f;
+		return Mathf.Clamp ( angle, min, max );
+	}
+}
---
+++ Assets/Scripts/ClickableEarth.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class ClickableEarth : MonoBehaviour
+{
+
+	#region vars
+
+	public Camera Camera;
+
+	public GameObject Cube;
+
+	#endregion
+
+	void Start ()
+	{
+
+
+	}
+
+	void Update ()
+	{
+
+
+	}
+
+	private void OnMouseUp ()
+	{
+		Ray ray = this.Camera.ScreenPointToRay ( Input.mousePosition );
+		
+		RaycastHit hit;
+		
+		if ( Physics.Raycast ( ray, out hit ) )
+		{
+			Transform objectHit = hit.transform;
+
+			Debug.Log ( "Object hit ! ObjectName: " + objectHit.name );
+
+			Instantiate ( this.Cube, hit.point, Quaternion.identity );
+
+			Debug.Log ( "Instantiated Cube at : " + hit.point );
+		}
+	}
+}
