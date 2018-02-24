1628a083e2d16288c563b18ea45a3ee656b88406
SanderJSA <31779371+SanderJSA@users.noreply.github.com>
2018-02-24 20:09:21 +0100

    Updated Main_Controller

--- Assets/Scripts/Main_Controller.cs
+++ Assets/Scripts/Main_Controller.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using JetBrains.Annotations;
 using UnityEngine;
@@ -7,19 +9,32 @@ using UnityEngine;
 public class Main_Controller : MonoBehaviour
 {
 
+	//info sur la partie
 	public static int time = 0;
 	public static int totalSane;
 	public static int totalInfected;
 	private int power;
 
+    //info sur le virus
 	private int transmiton = 0;
 	private int virulence = 0;
 	private int chanceToKill = 0;
 	private int tempRes;
 	private int HumidityRes;
 	
+	//Dictionnaire contenant toutes les info sur chaque competences:
+	//Utilisation: Description["Purge"] retourne un string qui est sa description
+	
+	public readonly Dictionary<String, String> Description = new Dictionary<String, String>
+	{
+		{"Localisation", "Lance la recherche du virus dans le pays selectionne "},
+		{"Oxygen", "Le virus se multiplie au contact de l'air, augmentant sa transmition"}
+	};
+	
+	
 	void Start ()
 	{
+		
 		//TODO appel de l'UI demandant a l'utilisateur de selectionner une region
 
 		totalSane = 0; //TODO fonction qui fait la somme de toute les populations de regions
