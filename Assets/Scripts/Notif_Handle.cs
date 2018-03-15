1de43f47ea63af17a901f1223de78f39eaa9d009
vertebarbe <35434346+vertebarbe@users.noreply.github.com>
2018-03-15 22:33:04 +0100

    added notification close script

---
+++ Assets/Scripts/Notif_Handle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Notif_Handle : MonoBehaviour {
+
+	public void closeNotification(){
+		GetComponent<Canvas> ().enabled = false;
+	}
+}
