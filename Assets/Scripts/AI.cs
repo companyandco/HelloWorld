25c1d258a63426c6f0c9127888a6bbbded43f315
SanderJSA <31779371+SanderJSA@users.noreply.github.com>
2018-05-03 01:14:55 +0200

    AI rework n1

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -33,33 +33,45 @@ public class AI : MonoBehaviour
 		i = 0;
 
 		if (!located)
+		{
 			located = Main_Controller_def.Localisation(
 				Main_Controller.Earth.regionlist[Random.Range(0, Main_Controller.symptoms.Count)]);
+		}
 		else
 		{
-			if (Main_Controller_def.foundTransmitions.Count != Main_Controller.transmitions.Count)
-				Main_Controller_def.ResearchTrans(Main_Controller.transmitions[Random.Range(0, Main_Controller.symptoms.Count)]);
+			Main_Controller_def.ResearchAntidote();
+			if (Main_Controller.transmitions.Count != 0)
+				
+			if (Main_Controller.symptoms.Count != 0 && !Main_Controller_def.ResearchTrans(Main_Controller.transmitions[Random.Range(0, Main_Controller.transmitions.Count)]))
+				Main_Controller_def.ResearchSymp(Main_Controller.symptoms[Random.Range(0, Main_Controller.symptoms.Count)]);
+		}
+
+
+		if (Main_Controller_def.foundSymptoms.Count == Main_Controller.symptoms.Count)
+		{
+			if (Main_Controller_def.foundTransmitions.Count == Main_Controller.transmitions.Count)
+				text.text = "L'IA recherche l'antidote";
+			else 
+				text.text = "L'IA a recherché tous les symptomes";
+		}
+		else
+		{
+			Debug.Log("trans found : " + Main_Controller_def.foundTransmitions.Count + "trans: " + Main_Controller.transmitions.Count);
+			Debug.Log("symp found : " + Main_Controller_def.foundSymptoms.Count + "symp: " + Main_Controller.symptoms.Count);
+			if (Main_Controller_def.foundTransmitions.Count == Main_Controller.transmitions.Count)
+				text.text = "L'IA a recherché toutes les transmitions";
 			else
 			{
-				if (Main_Controller_def.foundTransmitions.Count != Main_Controller.transmitions.Count)
-					Main_Controller_def.ResearchTrans(Main_Controller.transmitions[Random.Range(0, Main_Controller.symptoms.Count)]);
+				if (located)
+				{
+					text.text = "L'IA a localisé le virus";
+				}
 				else
-					Main_Controller_def.ResearchAntidote();
+				{
+					text.text = "L'IA tente de localiser le virus";
+				}
 			}
 		}
-
-
-
-
-
-
-
-		if ( Main_Controller_def.found )
-		{
-			this.text.text = "L'IA a localisé le virus: \n" + "vrai";
-		} else
-		{
-			this.text.text = "L'IA a localisé le virus: \n" + "faux";			
-		}
+		
 	}
 }
