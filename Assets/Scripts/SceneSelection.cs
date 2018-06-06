2d434034790d5edd45792d8dc100d92274bafa33
Dominique <Dominique57@users.noreply.github.com>
2018-06-06 11:40:02 +0200

    Added loading screen prefab and function in dedicated script
    Ai ajouté un prefab qui fait un loading screen + ai ajouté des fonctions dans sceneselector.cs afin de siplifier l'utilisation

--- Assets/Scripts/SceneSelection.cs
+++ Assets/Scripts/SceneSelection.cs
@@ -1,8 +1,54 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSelection : MonoBehaviour
 {
+	public GameObject LoadingScreen;
+
+
+	public void LoadNewScreen(string scene)
+	{
+		Instantiate(LoadingScreen);
+		LoadingScreen.SetActive(true);
+		StartCoroutine(LoadNewSceneCorutine(scene));
+	}
+	
+	public void LoadNewScreenImmediate(string scene)
+	{
+		Instantiate(LoadingScreen);
+		LoadingScreen.SetActive(true);
+		StartCoroutine(LoadNewSceneCorutineImmediate(scene));
+	}
+
+	
+	IEnumerator LoadNewSceneCorutine(string str)
+	{
+		//temps d'attente minimum pour pas faire des transitions epileptiques
+		yield return new WaitForSeconds(1);
+		
+		//charge la scene de maniere asynchrone
+		AsyncOperation async = SceneManager.LoadSceneAsync(str);
+
+		//tant que la scene a pas fini de charger on attend
+		while (!async.isDone)
+		{
+			yield return null;
+		}
+	}
+	
+	IEnumerator LoadNewSceneCorutineImmediate(string str)
+	{
+		//charge la scene de maniere asynchrone
+		AsyncOperation async = SceneManager.LoadSceneAsync(str);
+		
+		while (!async.isDone)
+		{
+			yield return null;
+		}
+	}
+
 	public void SendData ( bool hasWin )
 	{
 		char c = ( hasWin ) ? '1' : '2';
@@ -31,6 +77,7 @@ public class SceneSelection : MonoBehaviour
 		SceneManager.LoadScene ( "main_menu" );
 	}
 
+	
 	public GameObject MainMenuCanvas;
 	public GameObject SettingsCanvas;
 
---
+++ Assets/Scripts/UI/loadingCircle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadingCircle : MonoBehaviour {
+
+	private RectTransform rectComponent;
+	private float rotateSpeed = 200f;
+	public float rotateSpeedFactor;
+	
+	// Use this for initialization
+	void Start () {
+		rectComponent = GetComponent<RectTransform>();
+		if (rotateSpeedFactor == null)
+			rotateSpeedFactor = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(rotateSpeedFactor * rotateSpeed * Time.deltaTime);
+		rectComponent.Rotate(0f, 0f, -1 * rotateSpeedFactor * rotateSpeed * Time.deltaTime);
+	}
+}
