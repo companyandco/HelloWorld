5c7c47f2b57befe90a528d29f36e65a80fabdc26
Dominique <Dominique57@users.noreply.github.com>
2018-06-11 16:44:33 +0200

    Multiple Fixes
    Fixed default value of resolution not working due to false statement with height compared to width
    Also added default value for toggle if screen is fulsscreen or not
    Settingscontroller.cs
    
    in settingcanvas, changed scrollsensitivity to 20 making it ok
    
    Ortographe correction in signelplayer
    
    added gotobreak in projectosettings as company and added the default windows launcher to pre-select default settings such as resolution, fullscreen or graphic quality

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -7,8 +7,8 @@ using UnityEngine.UI;
 public class SettingsController : MonoBehaviour
 {
 	public AudioMixer AudioMixer;
-
 	public Dropdown ResolutionDropdown;
+	public Toggle FullscreenToggle;
 	
 	public void SET_VOLUME (float volume)
 	{
@@ -17,7 +17,7 @@ public class SettingsController : MonoBehaviour
 
 	public void SET_QUALITY ( int qualityIndex )
 	{
-		QualitySettings.SetQualityLevel ( qualityIndex );
+		QualitySettings.SetQualityLevel ( qualityIndex, true);
 	}
 
 	public void SET_FULLSCREEN ( bool isFullscreen )
@@ -43,6 +43,17 @@ public class SettingsController : MonoBehaviour
 	
 	void Start ()
 	{
+		if (FullscreenToggle != null)
+		{
+			if (Screen.fullScreen)
+				FullscreenToggle.isOn = true;
+			else
+				FullscreenToggle.isOn = false;
+		}
+		else
+			Debug.LogWarning("The toggle GameObject has not Been linked to the settingsController !");
+
+
 		this.resolutions = Screen.resolutions;
 		
 		this.ResolutionDropdown.ClearOptions ();
@@ -50,15 +61,13 @@ public class SettingsController : MonoBehaviour
 		List <string> options = new List <string> ();
 
 		int currentResolutionIndex = 0;
-		
 		for ( int i = 0; i < this.resolutions.Length; i++ )
 		{
 			string option = resolutions [i].width + " x " + resolutions [i].height;
 			options.Add ( option );
-
-			if ( resolutions [i].width == Screen.currentResolution.width && resolutions [i].height == Screen.currentResolution.width )
+			
+			if ( resolutions [i].width == Screen.currentResolution.width && resolutions [i].height == Screen.currentResolution.height )
 				currentResolutionIndex = i;
-
 		}
 
 		this.ResolutionDropdown.AddOptions ( options );
